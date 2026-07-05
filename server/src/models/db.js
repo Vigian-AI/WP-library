3baa5b2bc71d672aa4789a5bce9c2ec75dc612b4
ⱽᵍ` <pferdian147@gmail.com>
2026-07-05 19:45:47 +0700

    add docker compose mysql

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -9,7 +9,7 @@ class Database {
             host: process.env.DB_HOST || 'localhost',
             port: parseInt(process.env.DB_PORT) || 3306,
             user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || '',
+            password: process.env.DB_PASSWORD || 'root',
             database: process.env.DB_NAME || 'wp_library',
             waitForConnections: true,
             connectionLimit: 10,
